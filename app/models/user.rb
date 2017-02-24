5f586418d1fe6d02ea01c56d278d4487ce3c0785
Ankesh <aankeshsamarth@gmail.com>
2017-02-25 02:38:24 +0530

    minimum password length changed from 6 to 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  
+
   has_many :listings, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :trackable, :validatable, :confirmable, password_length: 8..15
 end
