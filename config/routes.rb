6da8e9f0246e41131625e24bacd69d2f32277f25
Ankesh <aankeshsamarth@gmail.com>
2017-02-25 16:34:05 +0530

    Added Seller Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
   resources :listings
+
   root 'listings#index'
+  get 'seller' => "listings#seller"
+
+  
   # static_pages
   controller :static_pages do
     get :trouble
   end
+
 end
